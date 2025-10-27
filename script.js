8bb8d00c3205be6622c22e2653f94604e67158e0
aldodenegri <aldodenegri00@gmail.com>
2025-10-27 18:56:16 +0100

    aggiunta specificazione della conversione in base 10 nei parseInt

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let km;
 let eta;
 do {
-    km = parseFloat(prompt("Inserisci chilometri:"));
+    km = parseFloat(prompt("Inserisci chilometri:"),10);
     if (isNaN(km) || km <= 0)
         alert("Per favore, inserisci un numero valido di chilometri.");
 } while (isNaN(km) || km <= 0);
 do {
-    eta = parseInt(prompt("Inserisci la tua età:"));
+    eta = parseInt(prompt("Inserisci la tua età:"),10);
     if (isNaN(eta) || eta <= 0)
         alert("Per favore, inserisci un'età valida.");
 } while (isNaN(eta) || eta <= 0);
