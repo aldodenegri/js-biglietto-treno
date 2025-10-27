58104d9357917e5c875515ae383a1529bce04d33
aldodenegri <aldodenegri00@gmail.com>
2025-10-27 18:39:58 +0100

    correzione formattazione script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let km = parseFloat(prompt("Inserisci chilometri:"));
-        let eta = parseInt(prompt("Inserisci la tua età:"));
-        const prezzoKm = 0.21;
-        let prezzoTotale = prezzoKm * km;
-        let prezzoScontato = prezzoTotale;
+let eta = parseInt(prompt("Inserisci la tua età:"));
+const prezzoKm = 0.21;
+let prezzoTotale = prezzoKm * km;
+let prezzoScontato = prezzoTotale;
 
-        if (eta < 18) {
-            prezzoScontato = prezzoTotale * 0.8; // 20% di sconto
-        } else if (eta >= 65) {
-            prezzoScontato = prezzoTotale * 0.6; // 40% di sconto
-        }
+if (eta < 18) {
+    prezzoScontato = prezzoTotale * 0.8; // 20% di sconto
+} else if (eta >= 65) {
+    prezzoScontato = prezzoTotale * 0.6; // 40% di sconto
+}
 
-        const divRisultato = document.getElementById("risultato-somma");
-        divRisultato.textContent = "Prezzo da pagare: € " + prezzoScontato.toFixed(2);
+const divRisultato = document.getElementById("risultato-somma");
+divRisultato.textContent = "Prezzo da pagare: € " + prezzoScontato.toFixed(2);
